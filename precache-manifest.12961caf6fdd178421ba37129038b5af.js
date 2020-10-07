a2cd97b857d31600709e08139de53417fac748a0
Emil Ari Minet <35972271+Ariminet@users.noreply.github.com>
2020-10-08 00:28:13 +0200

    deploy

---
+++ precache-manifest.12961caf6fdd178421ba37129038b5af.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab9cd34ec38feabc08def7765408ad64",
+    "url": "/Bageriet/index.html"
+  },
+  {
+    "revision": "a8cfb1e6417ba961d796",
+    "url": "/Bageriet/static/css/main.bbdf5f27.chunk.css"
+  },
+  {
+    "revision": "70d160074f2db2f2c120",
+    "url": "/Bageriet/static/js/2.afe0cb49.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Bageriet/static/js/2.afe0cb49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8cfb1e6417ba961d796",
+    "url": "/Bageriet/static/js/main.98341fa5.chunk.js"
+  },
+  {
+    "revision": "ee78a65833e5fa336d03",
+    "url": "/Bageriet/static/js/runtime-main.306ee913.js"
+  },
+  {
+    "revision": "50145685042b4df07a1fd19957275b81",
+    "url": "/Bageriet/static/media/OpenSans-Bold.50145685.ttf"
+  },
+  {
+    "revision": "78b08a68d05d5fabb0b8effd51bf6ade",
+    "url": "/Bageriet/static/media/OpenSans-BoldItalic.78b08a68.ttf"
+  },
+  {
+    "revision": "8bac22ed4fd7c8a30536be18e2984f84",
+    "url": "/Bageriet/static/media/OpenSans-ExtraBold.8bac22ed.ttf"
+  },
+  {
+    "revision": "73d6bb0d4f596a91992e6be32e82e3bc",
+    "url": "/Bageriet/static/media/OpenSans-ExtraBoldItalic.73d6bb0d.ttf"
+  },
+  {
+    "revision": "c7dcce084c445260a266f92db56f5517",
+    "url": "/Bageriet/static/media/OpenSans-Italic.c7dcce08.ttf"
+  },
+  {
+    "revision": "1bf71be111189e76987a4bb9b3115cb7",
+    "url": "/Bageriet/static/media/OpenSans-Light.1bf71be1.ttf"
+  },
+  {
+    "revision": "6943fb6fd4200f3d073469325c6acdc9",
+    "url": "/Bageriet/static/media/OpenSans-LightItalic.6943fb6f.ttf"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/Bageriet/static/media/OpenSans-Regular.629a55a7.ttf"
+  },
+  {
+    "revision": "33f225b8f5f7d6b34a0926f58f96c1e9",
+    "url": "/Bageriet/static/media/OpenSans-Semibold.33f225b8.ttf"
+  },
+  {
+    "revision": "73f7301a9cd7a086295401eefe0c998f",
+    "url": "/Bageriet/static/media/OpenSans-SemiboldItalic.73f7301a.ttf"
+  }
+]);
